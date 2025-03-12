e692e15766ce409fb9b3843e5346b1d63e5461e5
Grigorii <gory.axxxv@gmail.com>
2025-03-12 23:13:33 +0700

    перенос SearchBar в LazyColum

--- app/src/main/java/com/grigorii/couponsapp/compose/screens/CatalogScreen.kt
+++ app/src/main/java/com/grigorii/couponsapp/compose/screens/CatalogScreen.kt
@@ -12,13 +12,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.SearchBar
@@ -49,140 +46,153 @@ fun CatalogScreen(modifier: Modifier = Modifier) {
 
     Column(
         modifier = Modifier
-            .padding(start = 16.dp, end = 16.dp)
             .background(color = Color.White)
     ) {
-
-        HeaderSection()
-
-        LazyColumn(
+        Column(
             modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.Start
+                .padding(start = 16.dp, end = 16.dp)
         ) {
 
-            item {
-                SearchBar(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 16.dp),
-                    inputField = {
-                        SearchBarDefaults.InputField(
-                            query = text,
-                            onQueryChange = {
-                                text = it
-                            },
-                            onSearch = {
-                                active = false
-                            },
-                            expanded = active,
-                            onExpandedChange = { active = it },
-                            enabled = true,
-                            placeholder = {
-                                Text("Искать...")
-                            },
-                            leadingIcon = null,
-                            trailingIcon = {
-                                Icon(
-                                    Icons.Default.Search,
-                                    contentDescription = "favorite",
-                                    modifier = Modifier.size(24.dp)
-                                )
-                            },
-                            interactionSource = null,
-                        )
-                    },
-                    expanded = active,
-                    onExpandedChange = {
-                        active = it
-                    },
-                    shape = SearchBarDefaults.inputFieldShape,
-                    tonalElevation = SearchBarDefaults.TonalElevation,
-                    shadowElevation = SearchBarDefaults.ShadowElevation,
-                    windowInsets = SearchBarDefaults.windowInsets,
-                    content = { }
-                )
-            }
+            HeaderSection()
 
-            item {
-                FilterButton(modifier = Modifier.padding(top = 32.dp))
-            }
-
-            item {
-                val tempItemss = listOf(
-                    CardItemContent(
-                        title = "Консультации психолога",
-                        painter = painterResource(id = R.drawable.psycholog),
-                        imageDescription = "Консультации психолога",
-                        location = "г. Томск",
-                        price = "2000 руб.",
-                        validityPeriod = "sss"
-                    ),
-                    CardItemContent(
-                        title = "Занятия по танцам",
-                        painter = painterResource(id = R.drawable.sportclub),
-                        imageDescription = "Занятия по танцам",
-                        location = "г.Томск",
-                        price = "2000 руб.",
-                        validityPeriod = "sss"
-                    ),
-                    CardItemContent(
-                        title = "Консультации психолога",
-                        painter = painterResource(id = R.drawable.psycholog),
-                        imageDescription = "Консультации психолога",
-                        location = "г. Томск",
-                        price = "2000 руб.",
-                        validityPeriod = "sss"
-                    ),
-                    CardItemContent(
-                        title = "Занятия по танцам",
-                        painter = painterResource(id = R.drawable.sportclub),
-                        imageDescription = "Занятия по танцам",
-                        location = "г.Томск",
-                        price = "2000 руб.",
-                        validityPeriod = "sss"
-                    ),
-                    CardItemContent(
-                        title = "Консультации психолога",
-                        painter = painterResource(id = R.drawable.psycholog),
-                        imageDescription = "Консультации психолога",
-                        location = "г. Томск",
-                        price = "2000 руб.",
-                        validityPeriod = "sss"
-                    ),
-                    CardItemContent(
-                        title = "Занятия по танцам",
-                        painter = painterResource(id = R.drawable.sportclub),
-                        imageDescription = "Занятия по танцам",
-                        location = "г.Томск",
-                        price = "2000 руб.",
-                        validityPeriod = "sss"
-                    ),
-                    CardItemContent(
-                        title = "Консультации психолога",
-                        painter = painterResource(id = R.drawable.psycholog),
-                        imageDescription = "Консультации психолога",
-                        location = "г. Томск",
-                        price = "2000 руб.",
-                        validityPeriod = "sss"
-                    ),
-                    CardItemContent(
-                        title = "Занятия по танцам",
-                        painter = painterResource(id = R.drawable.sportclub),
-                        imageDescription = "Занятия по танцам",
-                        location = "г.Томск",
-                        price = "2000 руб.",
-                        validityPeriod = "sss"
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.Start
+            ) {
+                item {
+                    SearchBar(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        inputField = {
+                            SearchBarDefaults.InputField(
+                                query = text,
+                                onQueryChange = {
+                                    text = it
+                                },
+                                onSearch = {
+                                    active = false
+                                },
+                                expanded = active,
+                                onExpandedChange = { active = it },
+                                enabled = true,
+                                placeholder = {
+                                    Text("Искать...")
+                                },
+                                leadingIcon = null,
+                                trailingIcon = {
+                                    Icon(
+                                        Icons.Default.Search,
+                                        contentDescription = "favorite",
+                                        modifier = Modifier.size(24.dp)
+                                    )
+                                },
+                                interactionSource = null,
+                            )
+                        },
+                        expanded = active,
+                        onExpandedChange = {
+                            active = it
+                        },
+                        shape = SearchBarDefaults.inputFieldShape,
+                        tonalElevation = SearchBarDefaults.TonalElevation,
+                        shadowElevation = SearchBarDefaults.ShadowElevation,
+                        windowInsets = SearchBarDefaults.windowInsets,
+                        content = { }
                     )
-                )
+                }
 
-                ContentScale(tempItemss = tempItemss)
+                item {
+                    FilterButton(modifier = Modifier.padding(top = 32.dp))
+                }
+
+                item {
+                    val tempItemss = listOf(
+                        CardItemContent(
+                            title = "Консультации психолога",
+                            painter = painterResource(id = R.drawable.psycholog),
+                            imageDescription = "Консультации психолога",
+                            location = "г. Томск",
+                            price = "2000 руб.",
+                            validityPeriod = "sss"
+                        ),
+                        CardItemContent(
+                            title = "Занятия по танцам",
+                            painter = painterResource(id = R.drawable.sportclub),
+                            imageDescription = "Занятия по танцам",
+                            location = "г.Томск",
+                            price = "2000 руб.",
+                            validityPeriod = "sss"
+                        ),
+                        CardItemContent(
+                            title = "Консультации психолога",
+                            painter = painterResource(id = R.drawable.psycholog),
+                            imageDescription = "Консультации психолога",
+                            location = "г. Томск",
+                            price = "2000 руб.",
+                            validityPeriod = "sss"
+                        ),
+                        CardItemContent(
+                            title = "Занятия по танцам",
+                            painter = painterResource(id = R.drawable.sportclub),
+                            imageDescription = "Занятия по танцам",
+                            location = "г.Томск",
+                            price = "2000 руб.",
+                            validityPeriod = "sss"
+                        ),
+                        CardItemContent(
+                            title = "Консультации психолога",
+                            painter = painterResource(id = R.drawable.psycholog),
+                            imageDescription = "Консультации психолога",
+                            location = "г. Томск",
+                            price = "2000 руб.",
+                            validityPeriod = "sss"
+                        ),
+                        CardItemContent(
+                            title = "Занятия по танцам",
+                            painter = painterResource(id = R.drawable.sportclub),
+                            imageDescription = "Занятия по танцам",
+                            location = "г.Томск",
+                            price = "2000 руб.",
+                            validityPeriod = "sss"
+                        ),
+                        CardItemContent(
+                            title = "Консультации психолога",
+                            painter = painterResource(id = R.drawable.psycholog),
+                            imageDescription = "Консультации психолога",
+                            location = "г. Томск",
+                            price = "2000 руб.",
+                            validityPeriod = "sss"
+                        ),
+                        CardItemContent(
+                            title = "Занятия по танцам",
+                            painter = painterResource(id = R.drawable.sportclub),
+                            imageDescription = "Занятия по танцам",
+                            location = "г.Томск",
+                            price = "2000 руб.",
+                            validityPeriod = "sss"
+                        )
+                    )
+
+                    ContentScale(tempItemss = tempItemss)
+                }
+
+                item {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 47.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        ShowMoreButton()
+                    }
+                }
             }
         }
     }
-
 }
 
 @Composable
@@ -190,8 +200,7 @@ private fun HeaderSection() {
     Row(
         modifier = Modifier
             .background(Color(0xFFFFFBFE))
-            .fillMaxWidth()
-            .padding(),
+            .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center
     ) {
@@ -233,11 +242,36 @@ fun FilterButton(modifier: Modifier = Modifier) {
 }
 
 @Composable
+fun ShowMoreButton(modifier: Modifier = Modifier) {
+    OutlinedButton(
+        onClick = { },
+        modifier = modifier.height(40.dp)
+    ) {
+        Icon(
+            Icons.Default.Add,
+            contentDescription = "show more",
+            modifier = Modifier.size(18.dp)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = "Показать еще",
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Medium
+            )
+        )
+    }
+}
+
+
+@Composable
 fun ContentScale(tempItemss: List<CardItemContent>) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 32.dp),
+            .padding(top = 32.dp, bottom = 32.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
 
